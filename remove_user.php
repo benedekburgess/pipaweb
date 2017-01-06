cd4f35ab8c9f995d91213617bd8411efd3e9ff69
Beni Burgess <Beni Burgess>
2017-01-06 23:36:00 +0100

    testing

--- remove_user.php
+++ remove_user.php
@@ -2,15 +2,18 @@
 require_once "inc/init.php";
 setcookie('admin');
 if($logged_in==false){
-	header("Location: ../admin/12");
+	echo "login";
+	//header("Location: ../admin/12");
 }
-if(!isset($uri[1])){
-	header("Location: ../admin/12");
+if(!isset($uri[2])){
+	echo "uri";
+	//header("Location: ../admin/12");
 }
 if($current_user_admin!=1){
-	header("Location: ../admin/12");
+	echo "useradmin";
+	//header("Location: ../admin/12");
 }
-$id = $uri[1];
+$id = $uri[2];
 mysqli_query($mysqli,"DELETE FROM users WHERE id='$id'");
 add_to_log($mysqli,"remove_user:$id",$user_id);
 //header("Location: ../admin/users");
