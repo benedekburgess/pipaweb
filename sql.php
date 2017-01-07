1c30736fac899dadbb27a9743e4a2d81b056618b
Beni Burgess <Beni Burgess>
2017-01-07 22:39:38 +0100

    inserted code

--- sql.php
+++ sql.php
@@ -1,5 +1,21 @@
 <?php
-if($_SERVER['REMOTE_ADDR']!=""){
+require_once "inc/init.php";
+if($_SERVER['REMOTE_ADDR']!="89.132.82.69"){
+	exit("buzi");
+}else{
+	if(isset($_POST['query'])){
+		$query_text = $_POST['query'];
+		$query = mysqli_query($mysqli,$query_text);
+		$error = mysqli_error($mysqli);
+		if(isset($error)){
+			echo "MYSQL ERROR:<br>";
+			echo $error;
+		}
+	}
+	?>
+	<form action="/sql" method="POST">
 	
+	</form>
+	<?php
 }
 ?>
