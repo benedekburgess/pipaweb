5186c0f844f89ddf2284ab73505caed4cd8400a6
Beni Burgess <Beni Burgess>
2017-01-08 18:37:39 +0100

    added pipe counter

--- index.php
+++ index.php
@@ -24,8 +24,12 @@ if(isset($user_id)){
 }
 $vanpipa = false;
 $query = mysqli_query($mysqli,"SELECT * FROM pipe");
+$count = 0;
 while($row = mysqli_fetch_assoc($query)){
 	$timestamp = $row['ts'];
+	if($timestamp+86400<time()){
+		$count++;
+	}
 	$diff = (time()-$timestamp)/60;
 	if($diff>0 && $diff<15){
 		$type = $row['type'];
@@ -73,6 +77,7 @@ while($row = mysqli_fetch_assoc($query)){
 			}elseif($vanpipa=="meghalo"){
 				echo "Kezd meghalni - ".$type."<br><span style='font-size:14px;'>".gmdate("H:i", $ts+600+360)." -kor kezdődött</span>";
 			}
+			echo "<br><span style='font-size:14px;'>Az elmúlt 24 órában $count db pipa volt</span>";
 			?>
 		</aside>
 		<footer>
