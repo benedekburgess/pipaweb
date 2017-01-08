0f495aac53002a21223aaa0c1f8f253a76386e78
Beni Burgess <Beni Burgess>
2017-01-08 21:55:48 +0100

    asd

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if($uri[1]=="fb"){
 }
 if(isset($uri[1]) && $uri[1]!="" && $uri[1]!="fb"){
 	echo $uri[1].".php";
-	require_once $uri[1].".php";
+	include $uri[1].".php";
 	die();
 }
 setcookie('admin');
