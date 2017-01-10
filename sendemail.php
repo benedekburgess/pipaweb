b0824684328b854c6ccc75947a63b637cb841ca3
Beni Burgess <Beni Burgess>
2017-01-10 19:14:13 +0100

    email code

--- sendemail.php
+++ sendemail.php
@@ -5,7 +5,9 @@ setcookie("admin");
 if($logged_in==false){
 	header("Location: ../");
 }
-
+if(isset($uri[2]) $$ $uri[2]!=""){
+	$email = $uri[2];
+}
 $query = mysqli_query($mysqli,"SELECT * FROM emailek");
 $fasz = false;
 while($row = mysqli_fetch_assoc($query)){
@@ -16,7 +18,7 @@ while($row = mysqli_fetch_assoc($query)){
 if($fasz==false){
 	$msg = "LEGYEN PIPA\n$current_username";
 	$headers = 'From: info@pipa.ml' . "\r\n" . 'Reply-To: info@pipa.ml' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-	mail("benedekb97@gmail.com","Legyen pipa",$msg,$headers);
+	mail($email,"Legyen pipa",$msg,$headers);
 }
 header("Location: ../");
 ?>
