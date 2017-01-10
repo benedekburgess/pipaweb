eff200dafcc4dac6e04ccbb1b6b53db062c50c9d
Beni Burgess <Beni Burgess>
2017-01-10 16:59:37 +0100

    please

--- admin.php
+++ admin.php
@@ -370,7 +370,7 @@ while($row = mysqli_fetch_assoc($query)){
 				<h3>
 				<?php
 				$arrsize = count($uri);
-				if(($arrsize==4 && !is_numeric($uri[4])) || ($arrsize==5 && is_numeric($uri[4]))){
+				if(($arrsize==4 && !is_numeric($uri[3])) || ($arrsize==5 && is_numeric($uri[4]))){
 					echo $arrsize;
 					echo $uri[4];
 					$url = "/admin/log/".$group_by."/";
