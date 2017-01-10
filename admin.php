ea55894f0ee64e2404f82652b27284a5f66e953e
Beni Burgess <Beni Burgess>
2017-01-10 13:21:47 +0100

    asd

--- admin.php
+++ admin.php
@@ -368,7 +368,7 @@ while($row = mysqli_fetch_assoc($query)){
 				</tr>
 				<?php
 				$page2 = ($page-1)*15;
-				$query = mysqli_query($mysqli,"SELECT * FROM pipe LIMIT 15 OFFSET $page2");
+				$query = mysqli_query($mysqli,"SELECT * FROM pipe LIMIT 15 OFFSET $page2 ORDER BY ts DESC");
 				while($row = mysqli_fetch_assoc($query)){
 					$id = $row['id'];
 					$ts = $row['ts'];
