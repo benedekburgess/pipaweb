06898861f6d01f5cb7f0649a0e95f4e160979190
Beni Burgess <Beni Burgess>
2017-01-10 17:28:51 +0100

    asd

--- admin.php
+++ admin.php
@@ -383,7 +383,7 @@ while($row = mysqli_fetch_assoc($query)){
 				<h3>
 				<?php
 				$arrsize = count($uri);
-				if(($arrsize==4 && !is_numeric($uri[3])) || ($arrsize==5 && is_numeric($uri[4])) || (!isset($uri[4]) && $uri[3]=="user_id") || ($arrsize==5 && $uri[3]=="user_id")){
+				if(($arrsize==4 && !is_numeric($uri[3])) || ($arrsize==5 && is_numeric($uri[4]))){
 					$url = "/admin/log/".$group_by."/";
 				}elseif($arrsize==3){
 					$url = "/admin/log/";
