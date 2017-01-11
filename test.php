7251e7c418f20e44450640462096d50e439ab4b8
Beni Burgess <Beni Burgess>
2017-01-11 15:38:20 +0100

    added test notification page

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Notification test page</title>
+	</head>
+	<body>
+	</body>
+	<script type="text/javascript" src="/js/jquery.js"></script>
+	<script type="text/javascript" src="/js/main.js"></script>
+</html>
