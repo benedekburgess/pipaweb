7f9a25e3a6b738c42cb77464f6244803e91c2256
Beni Burgess <Beni Burgess>
2017-01-16 22:06:26 +0100

    asdf

--- login.php
+++ login.php
@@ -12,12 +12,14 @@ if(strlen($password)>99){
 if($username==""){
 	header("Location: /admin/1");
 }else{
-	$query = mysqli_query($mysqli,"SELECT * FROM easter_eggs WHERE trigger='$username'");
+	$query = mysqli_query($mysqli,"SELECT * FROM easter_eggs");
 	echo mysqli_error($mysqli);
 	exit();
 	while($row = mysqli_fetch_assoc($query)){
-		echo $szar = $row['lnk'];
-		exit();
+		if($row['trigger']==$username){
+			echo $szar = $row['lnk'];
+			exit();
+		}
 	}
 }
 $query = mysqli_query($mysqli,"SELECT * FROM users WHERE username='$username'");
